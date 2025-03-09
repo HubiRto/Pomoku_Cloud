c0fdf9fd9a5e7ac411891906d4f936c8167ad18c
hubirto <hubert.rybicki.hr@gmail.com>
2025-03-09 20:41:38 +0100

    Init backend

---
+++ one-dock-service/src/main/java/cloud/pomoku/onedockservice/OneDockServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.pomoku.onedockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDockServiceApplication.class, args);
+	}
+
+}
---
+++ one-dock-service/src/test/java/cloud/pomoku/onedockservice/OneDockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.pomoku.onedockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a098f8ea0cdebd771c8056de1d007639a1d86245
hubirto <hubert.rybicki.hr@gmail.com>
2025-03-10 14:22:28 +0100

    Add exception handler to max size file

--- file-service/src/main/java/cloud/pomoku/fileservice/handlers/GlobalExceptionHandler.java
+++ file-service/src/main/java/cloud/pomoku/fileservice/handlers/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package cloud.pomoku.fileservice.handlers;
 import cloud.pomoku.fileservice.dto.responses.ErrorResponse;
 import cloud.pomoku.fileservice.exceptions.AppException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -14,5 +16,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ErrorResponse> handleAppException(AppException ex) {
         return ResponseEntity.status(ex.getStatus()).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(new ErrorResponse(ex.getMessage()));
     }
 }
